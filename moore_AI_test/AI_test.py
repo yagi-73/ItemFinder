87fae30061bcb9950a20278bd2cd92a8ae04c165
Moore0917 <107467186+Moore0917@users.noreply.github.com>
2023-12-21 10:43:00 +0900

    Qiitaそのまま写した状態。

--- moore_AI_test/AI_test.py
+++ moore_AI_test/AI_test.py
@@ -1,3 +1,78 @@
-### This is moore's AI test file.
-if __name__ == '__main__':
-    print("骨格診断のテストをするファイルだよ！")
+#? This is moore's AI test file.
+#! Check!!! install this library.
+#? pip3 install matplotlib opencv-python tensorflow tensorflow-hub
+
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+
+
+class PoseEstimator:
+    """入力画像から骨格のキーポイントを返す。"""
+
+    def __init__(self) -> None:
+        # Download the model from TF Hub.
+        model = hub.load("https://tfhub.dev/google/movenet/singlepose/lightning/4")
+        self.movenet = model.signatures["serving_default"]
+
+    def predict(self, target_image: np.ndarray) -> np.ndarray:
+        """RGB画像の入力から、その画像に映る1人の骨格のキーポイントを返す。
+
+        Args:
+            target_image (np.ndarray): 処理対象の画像
+
+        Returns:
+            np.ndarray: 検出されたキーポイント
+        """
+        # 推論できるように画像の整形
+        image = tf.expand_dims(target_image, axis=0)
+        image = tf.cast(tf.image.resize_with_pad(image, 192, 192), dtype=tf.int32)
+        # Run model inference.
+        outputs = self.movenet(image)
+        # Output is a [1, 1, 17, 3] tensor.
+        keypoints = outputs["output_0"]
+
+        del outputs, image, target_image
+        return keypoints.numpy()
+
+    def draw_prediction_on_image(self, target_image: np.ndarray, keypoints: np.ndarray):
+
+        from util import draw_prediction_on_image
+
+        return draw_prediction_on_image(target_image, keypoints)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    import cv2
+
+    # 引数の設定
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("image_path", help="実験対象の画像へのパス")
+
+    args = parser.parse_args()
+
+    img = cv2.imread(args.image_path)
+
+    # モデルの初期化
+    pe = PoseEstimator()
+    # 画像のキーポイントを取得
+    keypoints = pe.predict(img)
+    print(keypoints)
+
+    # 実行結果を保存
+    drwaed_img = pe.draw_prediction_on_image(img, keypoints=keypoints)
+    cv2.imwrite(f"{args.image_path.split('.')[0]}_results.png", drwaed_img)
+
+    # 出力
+    h, w, _ = drwaed_img.shape
+    concat_img = cv2.hconcat([cv2.resize(img, (w, h)), drwaed_img])
+    cv2.imshow("smaple", concat_img)
+
+    # キーが押されるまで待ち続ける。
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
