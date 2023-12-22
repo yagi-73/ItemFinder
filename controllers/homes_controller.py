3bb4dadd202ce6e55a9df99c9d3eceaa66ee1db3
iput2021-someya <tk210247yua.someya@gmail.com>
2023-12-22 10:38:25 +0900

    撮影ボタンの作成、cssファイル作成

--- controllers/homes_controller.py
+++ controllers/homes_controller.py
@@ -4,3 +4,7 @@ homes_controller = Blueprint("homes_controller", __name__)
 @homes_controller.route("/")
 def top():
   return render_template("top.html")
+
+@homes_controller.route("itemFinder")
+def itemFinder():
+  return render_template("itemFinder.html")
