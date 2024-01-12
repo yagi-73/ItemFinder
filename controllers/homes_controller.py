fd1c4267457b2299cbb2f464c2c96f0cfe595fd7
iput2021-someya <tk210247yua.someya@gmail.com>
2024-01-12 10:07:27 +0900

    homes_controller.pyにlast_page.pyを追加

--- controllers/homes_controller.py
+++ controllers/homes_controller.py
@@ -4,3 +4,7 @@ homes_controller = Blueprint("homes_controller", __name__)
 @homes_controller.route("/")
 def top():
   return render_template("top.html")
+
+@homes_controller.route("last_page")
+def itemFinder():
+  return render_template("last_page.html")
