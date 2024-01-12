cc7281962f9a713ada241c30719f590ab0add255
Moore Sasaki <moore.sasaki2@gmail.com>
2024-01-12 12:13:52 +0900

    ヤギちゃんの変更箇所およびライブラリインポート整頓

--- controllers/homes_controller.py
+++ controllers/homes_controller.py
@@ -4,7 +4,7 @@ homes_controller = Blueprint("homes_controller", __name__)
 
 @homes_controller.route("/")
 def top():
-  return render_template("top.html")
+  return render_template("homes/top.html")
 
 @homes_controller.route("itemFinder")
 def itemFinder():
